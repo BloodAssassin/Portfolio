767333f7daf5502ba5893c3e7f59b133badc5686
BloodAssassin <rmilos65@gmail.com>
2024-07-06 19:29:52 +0200

    Update

--- Logic.js
+++ Logic.js
@@ -243,3 +243,9 @@ function ToggleMenu()
     navButton.classList.toggle('active');
     navMenu.classList.toggle('active');
 }
+
+const birth = new Date("2005-03-23");
+const diffTime = Math.abs(new Date() - birth);
+const diffYears = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365)); 
+
+document.getElementById("age").innerHTML = '('+ diffYears + ')';
